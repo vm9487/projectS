7e856f3639b07788aabfc000a57f1dd50afff8d3
vm9487 <vm9487@hotmail.com>
2021-12-13 09:53:25 +0800

    1213 tammy 3rd

--- doDelete.php
+++ doDelete.php
@@ -3,13 +3,40 @@ require_once ("../db-connect.php");
 
 $customerID=$_GET["customerID"];
 
-//$sql="DELETE FROM users WHERE id='$id'";
+$campOwnerID=$_GET["campOwnerID"];
+
+
+if (isset($_GET["customerID"])) {
+
+    //$sql="DELETE FROM users WHERE id='$id'";
 $sql="UPDATE customer_list SET customerValid=0 WHERE customerID='$customerID'";
 
 
 if ($conn->query($sql) === TRUE) {
-    echo "刪除資料完成<br>";
-//    header("location: user-list.php");
+    // echo "刪除資料完成<br>";
+   header("location: customer-list.php");
 } else {
     echo "刪除資料錯誤: " . $conn->error;
 }
+
+
+
+} elseif ($_GET["campOwnerID"]) {
+//$sql="DELETE FROM users WHERE id='$id'";
+$sql="UPDATE camp_owner_list	
+SET campOwnerValid
+=0 WHERE campOwnerID
+='$campOwnerID
+'";
+
+
+if ($conn->query($sql) === TRUE) {
+    // echo "刪除資料完成<br>";
+   header("location: campowner-list.php");
+} else {
+    echo "刪除資料錯誤: " . $conn->error;
+}
+
+
+}
+
